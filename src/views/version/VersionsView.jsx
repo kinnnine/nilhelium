d7b553282603a4b550698a73ae459a81ee9aa8b8
Supphakit Duanghoy <nakaharaml9@gmail.com>
2026-07-25 15:05:03 +0000

    views/versions: version placeholder

--- src/views/version/VersionsView.jsx
+++ src/views/version/VersionsView.jsx
@@ -14,11 +14,35 @@ export const VersionsView = () => {
                             <h6 class="max">Versions</h6>
                             <button class="circle transparent">
                                 <i>add_box</i>
+                                <span class="tooltip bottom">Create New Version</span>
                             </button>
                             <MenuMisc />
                         </nav>
                     </header>
                     {/* Header */}
+                    {/* Versions */}
+                    <main class="compact">
+                        <section>
+                            <article class="secondary-container">
+                                <div class="row">
+                                    <div class="max">
+                                        <h5>1.8.8_u53_-wasm-gc</h5>
+                                    </div>
+                                </div>
+                                <nav>
+                                    <button class="circle transparent">
+                                        <i>edit_square</i>
+                                        <span class="tooltip bottom">Edit</span>
+                                    </button>
+                                    <button class="circle transparent">
+                                        <i>delete</i>
+                                        <span class="tooltip bottom">Delete</span>
+                                    </button>
+                                </nav>
+                            </article>
+                        </section>
+                    </main>
+                    {/* Versions */}
                 </>
             )
         }
