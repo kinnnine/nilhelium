54418bdc613f4b63ca77ead8b3a24f71ba78dc2a
Supphakit Duanghoy <nakaharaml9@gmail.com>
2026-07-25 11:32:18 +0000

    workbox: run after vite build, cache include .map

--- workbox-config.js
+++ workbox-config.js
@@ -1,7 +1,7 @@
 module.exports = {
 	globDirectory: 'dist',
 	globPatterns: [
-		'**/*.{json,html,js,css,woff2,svg}'
+		'**/*.{json,html,js,css,woff2,svg,map}'
 	],
 	swDest: 'dist/sw.js',
 	ignoreURLParametersMatching: [
