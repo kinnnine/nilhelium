8761748522742e7b1452c4f42443779aa70fd180
Supphakit Duanghoy <nakaharaml9@gmail.com>
2026-07-25 16:22:06 +0000

    views/*edit: placeholder for instance and version edit pages

--- src/views/instance/InstanceEditView.jsx
+++ src/views/instance/InstanceEditView.jsx
@@ -5,7 +5,53 @@ export const InstanceEditView = () => {
         view: () => {
             return (
                 <>
-                    <h1>InstanceEditView</h1>
+                    {/* Header */}
+                    <header class="primary-container">
+                        <nav>
+                            <button onclick={() => window.history.back()} class="circle transparent">
+                                <i>arrow_back</i>
+                            </button>
+                            <h6 class="max">instance_name / New Instance</h6>
+                            <button class="circle transparent">
+                                <i>save</i>
+                            </button>
+                        </nav>
+                    </header>
+                    {/* Header */}
+                    {/* Edit Form */}
+                    <main class="compact">
+                        <section>
+                            <article class="transparent">
+                                <div class="field middle-align">
+                                    <nav>
+                                        <div class="max">
+                                            <h6>Instance Name</h6>
+                                            <div>Name of this instance</div>
+                                        </div>
+                                        <label class="field border">
+                                            <input type="text"></input>
+                                            <span></span>
+                                        </label>
+                                    </nav>
+                                </div>
+                                <div class="field middle-align">
+                                    <nav>
+                                        <div class="max">
+                                            <h6>Version</h6>
+                                            <div>Assigned Eaglercraft version for this instance</div>
+                                        </div>
+                                        <div class="field suffix border">
+                                            <select>
+                                                <option>1.8.8_u53_-wasm-gc</option>
+                                            </select>
+                                            <i>arrow_drop_down</i>
+                                        </div>
+                                    </nav>
+                                </div>
+                            </article>
+                        </section>
+                    </main>
+                    {/* Edit Form */}
                 </>
             )
         }
--- src/views/version/VersionEditView.jsx
+++ src/views/version/VersionEditView.jsx
@@ -5,7 +5,71 @@ export const VersionEditView = () => {
         view: () => {
             return (
                 <>
-                    <h1>VersionEditView</h1>
+                    {/* Header */}
+                    <header class="primary-container">
+                        <nav>
+                            <button onclick={() => window.history.back()} class="circle transparent">
+                                <i>arrow_back</i>
+                            </button>
+                            <h6 class="max">version_name / New Version</h6>
+                            <button class="circle transparent">
+                                <i>save</i>
+                            </button>
+                        </nav>
+                    </header>
+                    {/* Header */}
+                    {/* Edit Form */}
+                    <main class="compact">
+                        <section>
+                            <article class="transparent">
+                                <div class="field middle-align">
+                                    <nav>
+                                        <div class="max">
+                                            <h6>Version Name</h6>
+                                            <div>Name of this Eaglercraft version</div>
+                                        </div>
+                                        <label class="field border">
+                                            <input type="text"></input>
+                                            <span></span>
+                                        </label>
+                                    </nav>
+                                </div>
+                                <div class="field middle-align">
+                                    <nav>
+                                        <div class="max">
+                                            <h6>Assets EPK Data</h6>
+                                            <div>Encoded base64 string of the assets.epk file</div>
+                                        </div>
+                                        <label class="field border">
+                                            <input type="text"></input>
+                                            <span></span>
+                                        </label>
+                                    </nav>
+                                </div>
+                                <div class="field middle-align">
+                                    <nav>
+                                        <div class="max">
+                                            <h6>Assets EPK Identification</h6>
+                                            <div>Identify trusted and valid assets.epk against NilHelium's assets.epk sha256 checksum list</div>
+                                        </div>
+                                        <label class="switch">
+                                            <input type="checkbox"></input>
+                                            <span></span>
+                                        </label>
+                                    </nav>
+                                </div>
+                                <div class="field middle-align">
+                                    <nav>
+                                        <div class="max">
+                                            <h6>Assets EPK Identification Status</h6>
+                                            <div>Disabled</div>
+                                        </div>
+                                    </nav>
+                                </div>
+                            </article>
+                        </section>
+                    </main>
+                    {/* Edit Form */}
                 </>
             )
         }
