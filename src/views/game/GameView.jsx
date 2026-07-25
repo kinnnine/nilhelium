72e39b728af1abc5841c5808285c8435a49644c5
Supphakit Duanghoy <nakaharaml9@gmail.com>
2026-07-25 11:53:38 +0000

    views/gameview: prepare body as game_frame

--- src/views/game/GameView.jsx
+++ src/views/game/GameView.jsx
@@ -2,10 +2,14 @@ import m from 'mithril';
 
 export const GameView = () => {
     return {
+        oncreate: () => {
+            document.body.removeAttribute('class');
+            document.body.id = "game_frame";
+            document.body.style = "margin:0px;width:100%;height:100%;overflow:hidden;background-color:white;";
+        },
         view: () => {
             return (
                 <>
-                    <h1>GameView</h1>
                 </>
             )
         }
