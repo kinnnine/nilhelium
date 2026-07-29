abcb3e39b418c828c9cbfe9d29c2fb92a390333e
kinnnine <nakaharaml9@gmail.com>
2026-07-30 01:17:40 +0700

    src/libs: indexedDB structure changes, working removeFile

--- src/constants.js
+++ src/constants.js
@@ -1,4 +1,4 @@
 export const DB_NAME = "nilhelium";
-export const DB_STORE_NAME = "files";
+export const DB_STORE_NAME = "data";
 export const STORAGE_NAMESPACE_INSTANCES = "__nilhelium_instances";
 export const STORAGE_NAMESPACE_VERSIONS = "__nilhelium_versions";
--- src/libs/helpers/FileHelper.js
+++ src/libs/helpers/FileHelper.js
@@ -7,23 +7,22 @@ export const FileHelper = {
         ) {
             const req = indexedDB.open(db_name, 1);
             req.onerror = (e) => {
-                console.error("failed to init db.");
+                console.error("Failed to init indexedDB.");
             };
             req.onsuccess = (e) => {
                 this.db = e.target.result;
                 this.store_name = db_store_name;
             };
             req.onupgradeneeded = (e) => {
-                const store = e.currentTarget.result.createObjectStore(db_store_name,
-                    { keyPath: 'id', autoIncrement: true });
-                store.createIndex('uuid', 'uuid', { unique: true });
-                store.createIndex('slot_name', 'slot_name', { unique: false });
+                const store = e.currentTarget.result.createObjectStore(db_store_name, { keyPath: 'uuid' });
             };
         } else {
             throw new Error("Incomplete required parameters: db_name (string), db_store_name (string)");
         };
     },
     getStore(mode = 'read') {
+        if (this.db == null && this.store_name == null)
+            throw new Error("Incomplete indexedDB initialization");
         if (typeof mode != 'undefined') {
             try {
                 return this.db.transaction(this.store_name, mode)
@@ -39,44 +38,65 @@ export const FileHelper = {
         // TODO
     },
     addFile(slot_name, blob, uuid) {
+        if (this.db == null && this.store_name == null)
+            throw new Error("Incomplete indexedDB initialization.");
         if (typeof slot_name != 'undefined' &&
             typeof blob === 'object' &&
             typeof uuid != 'undefined'
         ) {
-            var obj = { uuid: uuid, slot_name: slot_name, blob: blob };
-            var store = this.getStore('readwrite');
-            var req;
             try {
-                req = store.add(obj);
+                const store = this.getStore('readwrite');
+                const getReq = store.get(uuid);
+                getReq.onsuccess = (e) => {
+                    const obj = e.target.result || { uuid: uuid, files: {} };
+                    obj.files[slot_name] = blob;
+                    const putReq = store.put(obj);
+                    putReq.onsuccess = () => {
+                        console.log("addFile insertion successful");
+                    };
+                    putReq.onerror = (e) => {
+                        console.error("addFile insertion error", e.error);
+                    };
+                };
+                getReq.onerror = (e) => {
+                    console.error("addFile fetch error", e.error);
+                };
             } catch (err) {
-                console.error(err);
                 throw err;
-            };
-            req.onsuccess = (e) => {
-                console.log("addFile insertion successful.");
-            };
-            req.onerror = (e) => {
-                console.error("addFile error", e.error);
             };
         } else {
             throw new Error("Incomplete required parameters: slot_name (string), blob (object), uuid (string)");
         };
     },
     removeFile(slot_name, uuid) {
+        if (this.db == null && this.store_name == null)
+            throw new Error("Incomplete indexedDB initialization.");
         if (typeof slot_name != 'undefined' &&
             typeof uuid != 'undefined'
         ) {
-            var store = this.getStore('readwrite');
-            var req = store.index('uuid');
-            req.get(uuid).onsuccess = (e) => {
-                if (typeof e.target.result == 'undefined') {
-                    console.error("No matching record found");
-                    return;
+            try {
+                const store = this.getStore('readwrite');
+                const getReq = store.get(uuid);
+                getReq.onsuccess = (e) => {
+                    const obj = e.target.result;
+                    delete obj.files[slot_name];
+                    if (Object.keys(obj.files).length === 0) {
+                        store.delete(uuid);
+                        return;
+                    };
+                    const putReq = store.put(obj);
+                    putReq.onsuccess = () => {
+                        console.log("removeFile removal successful");
+                    };
+                    putReq.onerror = (e) => {
+                        console.error("removeFile removal error", e.error);
+                    }
                 };
-                console.log("found.");
-            };
-            req.onerror = (e) => {
-                console.error("removeFile error", e.error);
+                getReq.onerror = (e) => {
+                    console.error("removeFile fetch error", e.error);
+                };
+            } catch (err) {
+                throw err;
             };
         } else {
             throw new Error("Incomplete required parameters: slot_name (string), uuid (string)");
