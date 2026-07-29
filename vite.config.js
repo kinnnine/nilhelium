f23a38aa8d8c708e8bc646b3967152e5f0418bba
kinnnine <nakaharaml9@gmail.com>
2026-07-30 05:16:14 +0700

    vite.config.js: set base, server and don't include sourcemap

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,18 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+    base: '/nilhelium/',
     esbuild: {
         jsx: 'transform',
         jsxFactory: 'm',
         jsxFragment: 'm.Fragment',
+    },
+    server: {
+        host: '127.0.0.1',
+        port: 3000,
+    },
+    build: {
+        outDir: 'dist',
+        sourcemap: false,
     },
 });
