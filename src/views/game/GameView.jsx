c761a41abb13e8686c7175f27995e7dfd95c59db
kinnnine <nakaharaml9@gmail.com>
2026-07-30 05:19:24 +0700

    src/views: oncreate async due to wait for epw file

--- src/views/game/GameView.jsx
+++ src/views/game/GameView.jsx
@@ -1,11 +1,37 @@
 import m from 'mithril';
+import { InstanceManager } from "../../libs/InstanceManager";
+import { VersionManager } from "../../libs/VersionManager";
 
 export const GameView = () => {
     return {
-        oncreate: () => {
+        oninit: (vnode) => {
+            vnode.state.instanceUUID = m.route.param('uuid');
+            vnode.state.instanceName = "";
+            vnode.state.instanceVersion = "";
+            vnode.state.instance = null;
+            vnode.state.version = null;
+            const instance = InstanceManager.getInstance(vnode.state.instanceUUID);
+            if (instance != null) {
+                const version = VersionManager.getVersion(instance.version);
+                if (version != null) {
+                    vnode.state.instanceName = instance.name;
+                    vnode.state.instanceVersion = version.name;
+                    vnode.state.instance = instance;
+                    vnode.state.version = version;
+                } else {
+                    throw new Error("Version failure");
+                };
+            } else {
+                throw new Error("Instance failure");
+            };
+        },
+        oncreate: async (vnode) => {
+            document.title = vnode.state.instanceName + ' (' + vnode.state.instanceVersion + ')';
             document.body.removeAttribute('class');
             document.body.id = "game_frame";
             document.body.style = "margin:0px;width:100%;height:100%;overflow:hidden;background-color:white;";
+            const blob = await VersionManager.getEPWFile(vnode.state.instance.version);
+            console.log(blob);
         },
         view: () => {
             return (
